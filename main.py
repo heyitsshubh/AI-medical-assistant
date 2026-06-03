6dc46591abec5c9170e96c73a1518f49aa064bf9
unknown <shubhgupta4102@gmail.com>
2026-06-03 17:52:36 +0530

    added logger,cors,middlewares

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from ai-medical-assistant!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from server.middlewares.exception_handlers import catch_exception_middleware
 
 
-if __name__ == "__main__":
-    main()
+
+app=FastAPI(title="Medical Assistant API",description="API for AI Medical Assistant Chatbot")
+app.add_middleware(catch_exception_middleware)
+
+# CORS Setup
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
