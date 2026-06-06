d6d6c77cd34e179b0614e9f0c26a571b412b5e0c
unknown <shubhgupta4102@gmail.com>
2026-06-06 14:29:51 +0530

    server part completed,client part started

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from server.middlewares.exception_handlers import catch_exception_middleware
-from routes.upload_pdfs import router as upload_router
-from routes.ask_question import router as ask_router
+from server.routes.upload_pdfs import router as upload_router
+from server.routes.ask_question import router as ask_router
 
 
-app=FastAPI(title="Medical Assistant API",description="API for AI Medical Assistant Chatbot")
-app.add_middleware(catch_exception_middleware)
+app = FastAPI(
+    title="Medical Assistant API",
+    description="API for AI Medical Assistant Chatbot"
+)
+
+
+# Exception Middleware
+app.middleware("http")(catch_exception_middleware)
+
 
 # CORS Setup
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
-# 1. upload pdfs documents
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Medical AI API Running"
+    }
+
+
+# 1. upload pdf documents
 app.include_router(upload_router)
+
+
 # 2. asking query
-app.include_router(ask_router)
+app.include_router(ask_router)  
--- server/models/load_vectorstore.py
+++ server/models/load_vectorstore.py
@@ -5,13 +5,22 @@ from dotenv import load_dotenv
 from tqdm.auto import tqdm
 from pinecone import Pinecone, ServerlessSpec
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+
 
 load_dotenv()
 
-GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
-PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+
+
+print(
+    "PINECONE KEY:",
+    PINECONE_API_KEY
+)
 PINECONE_ENV="us-east-1"
 PINECONE_INDEX_NAME="medicalindex"
 
